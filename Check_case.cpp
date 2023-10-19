1346787ea4b91d001d026456e9ae22a7e76cbd9a
Sarthak <saarthaksach@gmail.com>
2023-10-19 10:32:16 +0530

    adding solutions, formatting docs for cleaner code

--- Check_case.cpp
+++ Check_case.cpp
@@ -1,22 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    char ch;
-    cin>>ch;
-    if(ch>=65 && ch<=90)
-    {
-        cout<<"1";
-    }
-    else if(ch>=97 && ch<=122)
-    {
-        cout<<"0";
-    }
-    else
-    {
-        cout<<"-1";
-    }
-    return 0;
+  char ch;
+  cin >> ch;
+  if (ch >= 65 && ch <= 90)
+  {
+    cout << "1";
+  }
+  else if (ch >= 97 && ch <= 122)
+  {
+    cout << "0";
+  }
+  else
+  {
+    cout << "-1";
+  }
+  return 0;
 }
 
 /*
