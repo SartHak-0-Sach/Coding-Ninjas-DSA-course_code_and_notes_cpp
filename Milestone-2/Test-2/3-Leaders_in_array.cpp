3006e3fdc6f85ba359b07facd0f24d0465c07c64
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-16 18:38:23 +0530

    Add solutions of milestone-2 test-2

--- Milestone-2/Test-2/3-Leaders_in_array.cpp
+++ Milestone-2/Test-2/3-Leaders_in_array.cpp
@@ -1,69 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <climits>
+using std::cout;
 
-// function check whether a number is prime or not
-bool isPrime(int n)
+void Leaders(int arr[],int len)
 {
-    // Corner case
-    if (n <= 1)
-        return false;
+	int save[len];
+	int largest = INT_MIN;
+	int j = 0;
+	for(int i = len-1; i>=0; i--)
+	{
+		if(arr[i]>=largest)
+		{
+			save[j] = arr[i];
+			largest = arr[i];
+			j++;
+		}
+	}
 
-    // Check from 2 to n-1
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-
-    return true;
+	for(int i = j-1; i>=0; i--)
+	{
+		cout<<save[i]<<" ";
+	}
 }
-
-// Function to print primes
-void printPrime(int n)
-{
-    for (int i = 2; i <= n; i++)
-        if (isPrime(i))
-            cout << i << endl;
-}
-
-// Driver Code
-int main()
-{
-    int n;
-    cin >> n;
-    printPrime(n);
-}
-
-// sieve of erathonesis code for finding all primes from 1 to N
-// #include <bits/stdc++.h>
-// using namespace std;
-// const int MAX_SIZE=1e6+5;
-// vector<int>isprime(MAX_SIZE,true);
-// vector<int>prime;
-// vector<int>SPF(MAX_SIZE);
-// void manipulated_seive(int N)
-// {
-//     isprime[0]=isprime[1]=false;
-//     for(int i=2;i<N;i++)
-//     {
-//         if(isprime[i])
-//         {
-//             prime.push_back(i);
-//             SPF[i]=i;
-//         }
-//         for(int j=0;j<prime.size()&&i*prime[j]<N&&prime[j]<=SPF[i];j++)
-//         {
-//             isprime[i*prime[j]]=false;
-//             SPF[i*prime[j]]=prime[j];
-//         }
-//     }
-// }
-// int main()
-// {
-//     int n;
-//     cin>>n;
-//     manipulated_seive(n);
-//     for(int i=0;i<prime.size()&&prime[i]<=n;i++)
-//     {
-//         cout<<prime[i]<<" ";
-//     }
-//     return 0;
-// }
