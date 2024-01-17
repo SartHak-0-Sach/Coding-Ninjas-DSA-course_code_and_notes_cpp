7fadcc55c94c86900f455bd8d376160c33617d46
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-17 13:11:59 +0530

    Add new solutions of lec-11

--- Milestone-2/Lec-11/09-Check_permutation.cpp
+++ Milestone-2/Lec-11/09-Check_permutation.cpp
@@ -1,26 +1,30 @@
-#include <bits/stdc++.h> 
 #include <iostream>
-#include <cstring>
-using namespace std;
+#include <string>
+#include <array>
 
+bool areAnagram(const std::string &str1, const std::string &str2) {
+    // Assuming ASCII characters, you may need a larger array for extended character sets.
+    const int CHAR_COUNT = 256; // Assuming ASCII characters
+    std::array<int, CHAR_COUNT> hashArray = {0};
 
-void printSubstrings(string input) {
-    int count = 0;
-    for(int i = 0; i < input.size(); i++)
-    {
-        int j = i+count;
-        count++;
-        for(int k = i; k<=j; k++)
-        {
-            cout<<input[k];
-        }
-        cout<<endl;
+    // Increment count for characters in str1
+    for (char ch : str1) {
+        hashArray[ch]++;
     }
+
+    // Decrement count for characters in str2
+    for (char ch : str2) {
+        hashArray[ch]--;
+    }
+
+    // Check if all counts are zero
+    for (int count : hashArray) {
+        if (count != 0) {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-int main() {
-    string input;
-    getline(cin, input);
-    printSubstrings(input);
-    return 0;
-}
+// can also be done using sorting and comparing each element using one loop but this method is faster
