64299f26c227b15013d97d80e75cd790270fdca5
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-01-21 23:35:13 +0530

    Update 11-Remove_consecutive_duplicates.cpp

--- Milestone-2/Lec-10/11-Remove_consecutive_duplicates.cpp
+++ Milestone-2/Lec-10/11-Remove_consecutive_duplicates.cpp
@@ -1,31 +1,21 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-bool checkPalindrome(string str) {
-    int i = 0;
-    int j = str.size()-1;
-    bool flag = true;
-    while(i<j)
-    {
-        if(str[i]!=str[j])
-        {
-            flag = false;
-            break;
-        }
-        i++;
-        j--;
-    }
-    return flag;
-}
-
-int main(int argc, char const *argv[])
+void removeConsecutiveDuplicates(char input[])
 {
-  string str;
-  cin>>str;
-  if(checkPalindrome(str))
-  cout<<"Palindrome"<<endl;
-  else
-  cout<<"Not a Palindrome"<<endl;
-  return 0;
+    for (int i = 1; input[i] != '\0'; i++)
+    {
+        if (input[i] == input[i - 1])
+        {
+            input[i - 1] = ' ';
+        }
+    }
+    int space = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        if (input[i] != ' ')
+        {
+            char tmp = input[i];
+            input[i] = input[space];
+            input[space] = tmp;
+            space++;
+        }
+    }
 }
