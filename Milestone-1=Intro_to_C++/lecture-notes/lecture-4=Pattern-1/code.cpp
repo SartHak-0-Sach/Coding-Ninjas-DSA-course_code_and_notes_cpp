8188bfcd772104347b483085005da471d6d3b687
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-13 22:43:19 +0530

    Update code.cpp

--- Milestone-1=Intro_to_C++/lecture-notes/lecture-4=Pattern-1/code.cpp
+++ Milestone-1=Intro_to_C++/lecture-notes/lecture-4=Pattern-1/code.cpp
@@ -1,1 +1,72 @@
-// refer to questions folder to see all the patterns and application of loops
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int j = 1;
+        while (j <= n)
+        {
+            cout << "*";
+            j++;
+        }
+        cout << endl;
+        i++;
+    }
+
+    /*
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int j=1;
+        while(j<=n){
+            cout<<n-j+1;
+            j++;
+        }
+        cout<<endl;
+        i++;
+    }
+    */
+
+    /*
+    int n;
+     cin>>n;
+     int i=1;
+     int k=1;
+     while(i<=n){
+         int j=1;
+         while(j<=i){
+             cout<<k;
+             k++;
+             j++;
+         }
+         cout<<endl;
+         i++;
+
+
+     }
+    */
+
+    /*
+    int n;
+     cin>>n;
+     int i=1;
+     int k=1;
+     while(i<=n){
+         int j=1;
+         char startChar='A'+i-1;
+         while(j<=n){
+             char ch=startChar+j-1;
+             cout<<ch;
+
+             j++;
+         }
+         cout<<endl;
+         i++;
+     }
+    */
+}
