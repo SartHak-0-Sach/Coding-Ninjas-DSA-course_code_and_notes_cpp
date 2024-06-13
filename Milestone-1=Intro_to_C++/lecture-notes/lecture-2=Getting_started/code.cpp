7a7457b9a1430b21ba3f83be631dd11eae377299
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-13 22:43:06 +0530

    Update code.cpp

--- Milestone-1=Intro_to_C++/lecture-notes/lecture-2=Getting_started/code.cpp
+++ Milestone-1=Intro_to_C++/lecture-notes/lecture-2=Getting_started/code.cpp
@@ -3,7 +3,56 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello, World!"; // first program
+    cout << "Hello" << "\n"; // first program
+
+    /*
+    int a;
+    int b;
+
+    cout << "Enter the numbers" << endl;
+    cin >> a >> b;
+
+    int c = a + b;
+
+    cout << c << endl;
+    */
+
+    /*
+    char a = 234354;
+     cout << a << endl;
+     unsigned int b = -123;
+     cout << b << endl;
+     */
+
+    /*
+    int f;
+    cout << "Enter Fah Value" << endl;
+    cin >> f;
+    int c = (5.0/9) * (f - 32);
+    c = (5 * (f - 32))/9;
+
+    int r = 10 % 3;
+
+    cout << 10 % 3 << endl;
+    cout << c << endl;
+
+    int a;
+    int b;
+    cout << "Enter a and b" << endl;
+    cin >> a >> b;
+
+    bool isEqual = (a == b);
+    bool isAGreater = (a > b);
+    bool isALess = (a < b);
+    cout << "Are they Equal " << isEqual << endl;
+    cout << "is A greater " << isAGreater << endl;
+    cout << "is A less " << isALess << endl;
+
+    bool third = isEqual && isAGreater;
+    bool fourth = isEqual || isAGreater;
+
+    cout << "Not equal " << !isEqual << endl;
+    */
 
     int a = 13;
     float b = 234.778;
