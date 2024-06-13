84649fb7e0bb46c3da64cabe6d2bd96f90ff4414
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-13 22:43:14 +0530

    Update code.cpp

--- Milestone-1=Intro_to_C++/lecture-notes/lecture-5=Pattern-2/code.cpp
+++ Milestone-1=Intro_to_C++/lecture-notes/lecture-5=Pattern-2/code.cpp
@@ -1,1 +1,69 @@
-// refer to questions folder to see all the patterns and application of loops
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int spaces = 1;
+        while (spaces <= n - i)
+        {
+            cout << ' ';
+            spaces = spaces + 1;
+        }
+
+        int stars = 1;
+        while (stars <= i)
+        {
+            cout << '*';
+            stars = stars + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+
+    /*
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n) {
+        int j = 1;
+        while (j <= n - i + 1) {
+            cout << '*';
+            j = j + 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+
+    /*
+    int n;
+    cin >> n;
+    int i = 1;
+    while (i <= n) {
+        int spaces = 1;
+        while (spaces <= n - i) {
+            cout << ' ';
+            spaces = spaces + 1;
+        }
+
+        int j = 1;
+        while (j <= i) {
+            cout << j;
+            j = j + 1;
+        }
+
+        j = i - 1;
+        while (j >= 1) {
+            cout << j;
+            j = j - 1;
+        }
+        cout << endl;
+        i = i + 1;
+    }
+    */
+}
