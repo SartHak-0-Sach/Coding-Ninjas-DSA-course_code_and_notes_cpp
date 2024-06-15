af7bbf0342a01ad46d924fb2b63a2bfa743ce715
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:35:06 +0530

    Update code.cpp

--- Milestone-1=Intro_to_C++/lecture-notes/lecture-6=Operators_and_for_loop/code.cpp
+++ Milestone-1=Intro_to_C++/lecture-notes/lecture-6=Operators_and_for_loop/code.cpp
@@ -4,23 +4,48 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
     int n;
-    cin>>n;
-    for(int i = 0; i<n; i++) // for loops
+    cin >> n;
+    for (int i = 0; i < n; i++) // for loops
     {
         int a, b;
-        cin>>a>>b;
-        if(a>b)
+        cin >> a >> b;
+        if (a > b)
         {
-            cout<<">"<<endl; // operators
+            cout << ">" << endl; // operators
         }
-        else if(a<b)
+        else if (a < b)
         {
-            cout<<"<"<<endl;
+            cout << "<" << endl;
         }
         else
         {
-            cout<<"="<<endl;
+            cout << "=" << endl;
         }
     }
+
+    int ror = 15 | 30;
+    int rand = 15 & 30;
+    int rnot = ~15;
+    int rxor = 15 ^ 30;
+    int rls = 15 << 2;
+    int rrs = 15 >> 2;
+
+    cout << ror << endl;
+    cout << rand << endl;
+    cout << rnot << endl;
+    cout << rxor << endl;
+    cout << rls << endl;
+    cout << rrs << endl;
+
+    char c;
+    c = cin.get();
+    int count = 0;
+    while (c != '$')
+    {
+        count++;
+        c = cin.get();
+    }
+    cout << count << endl;
+
     return 0;
 }
