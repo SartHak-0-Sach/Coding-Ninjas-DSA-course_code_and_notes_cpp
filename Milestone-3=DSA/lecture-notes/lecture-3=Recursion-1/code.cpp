f5fe2490913cf3015a6121a35cc54de7f2cde7e3
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:36:05 +0530

    Update code.cpp

--- Milestone-3=DSA/lecture-notes/lecture-3=Recursion-1/code.cpp
+++ Milestone-3=DSA/lecture-notes/lecture-3=Recursion-1/code.cpp
@@ -15,9 +15,81 @@ int fib(int n)
     return fib(n-1) + fib(n-2);
 }
 
+bool is_sorted(int a[], int size)
+{
+    if (size == 0 || size == 1)
+    {
+        return true;
+    }
+
+    if (a[0] > a[1])
+    {
+        return false;
+    }
+    bool isSmallerSorted = is_sorted(a + 1, size - 1);
+    return isSmallerSorted;
+    /*if (isSmallerSorted) {
+        return true;
+    } else {
+        return false;
+    }*/
+}
+
+bool is_sorted2(int a[], int size)
+{
+    if (size == 0 || size == 1)
+    {
+        return true;
+    }
+
+    bool isSmallerOutput = is_sorted2(a + 1, size - 1);
+    if (!isSmallerOutput)
+    {
+        return false;
+    }
+    if (a[0] > a[1])
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+
+int factorial(int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    int smallOutput = factorial(n - 1);
+    int output = n * smallOutput;
+    return output;
+}
+
+int fib2(int n)
+{
+    if (n == 0)
+    {
+        return 0;
+    }
+
+    if (n == 1)
+    {
+        return 1;
+    }
+
+    int smallOutput1 = fib(n - 1);
+    int smallOutput2 = fib(n - 2);
+    return smallOutput1 + smallOutput2;
+}
+
 int main(int argc, char const *argv[])
 {
     int n = 12;
     cout << "Nth number in fibonacci sequence is: " << fib(n) << endl;
+    cout << factorial(4) << endl;
+    cout << fib2(3) << endl;
     return 0;
 }
