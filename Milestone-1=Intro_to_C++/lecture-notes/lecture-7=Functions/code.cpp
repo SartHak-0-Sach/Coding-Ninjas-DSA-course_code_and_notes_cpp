a6b90e9b28550b75a1652e730787c555423bccc9
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:35:09 +0530

    Update code.cpp

--- Milestone-1=Intro_to_C++/lecture-notes/lecture-7=Functions/code.cpp
+++ Milestone-1=Intro_to_C++/lecture-notes/lecture-7=Functions/code.cpp
@@ -3,9 +3,36 @@ using namespace std;
 
 int addNum(int a, int b) {return a + b;}
 
+int fact(int n)
+{
+    int ans = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        ans = ans * i;
+    }
+    return ans;
+}
+
+void increment(int a)
+{
+    a = a + 1;
+}
+
 int main() {
     int x = 5;
     int y = 10;
     cout << addNum(x, y) << endl;
+
+    int a;
+    cin >> a;
+    int output = fact(a);
+    // cout << ans << endl;
+    // cout << n << endl;
+    cout << output << endl;
+
+    a = 10;
+    increment(a);
+    cout << a << endl;
+
     return 0;
 }
