c4c7b1b73a451de87f2f9e1bf59db216568cc794
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:35:16 +0530

    Update code.cpp

--- Milestone-2=Intro_to_C++/lecture-notes/lecture-09=Searching_and_sorting/code.cpp
+++ Milestone-2=Intro_to_C++/lecture-notes/lecture-09=Searching_and_sorting/code.cpp
@@ -2,6 +2,29 @@
 #include <vector>
 using namespace std;
 
+int binarySearch(int arr[], int n, int x)
+{
+    int start = 0, end = n - 1;
+    while (start <= end)
+    {
+        int mid = (start + end) / 2;
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+        else if (x < arr[mid])
+        {
+            end = mid - 1;
+        }
+        else
+        {
+            start = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
 void binarySearch()
 {
     int arr[15]={1,29,3,644,55,36,7,8,98,10,161,12,114,15,13}; // searching
@@ -49,8 +72,137 @@ int* bubbleSort()
     return arr;
 }
 
+void selectionSort(int input[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        // Find min element in the array
+        int min = input[i], minIndex = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (input[j] < min)
+            {
+                min = input[j];
+                minIndex = j;
+            }
+        }
+        // Swap
+        int temp = input[i];
+        input[i] = input[minIndex];
+        input[minIndex] = temp;
+    }
+}
+
+void printArray(int input[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        for (int i = 0; i < n - 1 - j; i++)
+        {
+            if (arr[i] > arr[i + 1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i + 1];
+                arr[i + 1] = temp;
+            }
+        }
+    }
+}
+
+void insertionSort(int arr[], int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        int current = arr[i];
+        int j;
+        for (j = i - 1; j >= 0; j--)
+        {
+            if (current < arr[j])
+            {
+                arr[j + 1] = arr[j];
+            }
+            else
+            {
+                break;
+            }
+        }
+        arr[j + 1] = current;
+    }
+}
+
+void printArray(int input[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+}
+
 int main(int argc, char const *argv[])
 {
     int arr[15]={1,29,3,644,55,36,7,8,98,10,161,12,114,15,13};
+    // Take array input from the user
+    int n;
+    cin >> n;
+
+    int input[100];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+
+    int x;
+    cin >> x;
+
+    cout << binarySearch(input, n, x) << endl;
+
+    int input[] = {3, 1, 6, 9, 0, 4};
+    selectionSort(input, 6);
+    for (int i = 0; i < 6; i++)
+    {
+        cout << input[i] << " ";
+    }
+    cout << endl;
+
+    // Take array input from the user
+    int n;
+    cin >> n;
+
+    int input[100];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+
+    bubbleSort(input, n);
+
+    printArray(input, n);
+
+    // Take array input from the user
+    int n;
+    cin >> n;
+
+    int input[100];
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> input[i];
+    }
+
+    insertionSort(input, n);
+
+    printArray(input, n);
     return 0;
 }
