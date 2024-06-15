8bd54ac5a4e6bbfdda8216d320591902fef850be
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:35:50 +0530

    Update code.cpp

--- Milestone-3=DSA/lecture-notes/lecture-1=Pointers/code.cpp
+++ Milestone-3=DSA/lecture-notes/lecture-1=Pointers/code.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+void increment1(int **p)
+{
+    p = p + 1;
+}
+void increment2(int **p)
+{
+    *p = *p + 1;
+}
+void increment3(int **p)
+{
+    **p = **p + 1;
+}
+
 int main(int argc, char const *argv[])
 {
     int a = 12;
@@ -32,5 +45,66 @@ int main(int argc, char const *argv[])
     cout << "Value at address pointed by c: " << **c << endl;
     (*c)++;
     cout << "Value at address pointed by c: " << **c << endl;
+
+    int i = 10;
+    int *p = &i;
+    cout << p << endl;
+    // p = p+1 ;
+    p = p + 2;
+    cout << p << endl;
+    p = p - 2;
+    cout << p << endl;
+
+    double d = 102.3;
+    double *dp = &d;
+    cout << dp << endl;
+    dp++;
+    cout << dp << endl;
+
+    int a1[10];
+    cout << a1 << endl;
+    cout << &a1[0] << endl;
+    a1[0] = 5;
+    cout << *a1 << endl;
+    cout << *(a1 + 1) << endl;
+    int *p = &a1[0];
+    cout << a1 << endl;
+    cout << p << endl;
+
+    cout << &p << endl;
+    cout << &a1 << endl;
+
+    cout << sizeof(p) << endl;
+    cout << sizeof(a1) << endl;
+
+    p = p + 1;
+    // a1=a1+1; // not allowed
+
+    // char str[] ="abcde";
+    // char *pstr ="abcde";
+    int a[] = {1, 2, 3};
+    char b2[] = "abc";
+    cout << a << endl;
+    cout << b2 << endl;
+    char *c = &b2[0];
+    cout << c << endl;
+    char c1 = 'a';
+    char *pc = &c1;
+    cout << c1 << endl;
+    cout << pc << endl;
+
+    int i = 10;
+    int *p = &i;
+    int **p2 = &p;
+    cout << p2 << endl;
+    cout << &p << endl;
+
+    cout << p << endl;
+    cout << *p2 << endl;
+    cout << &i << endl;
+
+    cout << i << endl;
+    cout << *p << endl;
+    cout << **p2 << endl;
     return 0;
 }
