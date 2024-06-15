eb73d06d22dd176969d15a12219a57c7968eba89
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:36:39 +0530

    Update code.cpp

---
+++ Milestone-5=DSA/lecture-notes/lecture-16=Tries_and_huffman_coding/code.cpp
@@ -0,0 +1,14 @@
+class TrieNode
+{
+public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+
+    TrieNode(char data)
+    {
+        this->data = data;
+        children = new TrieNode *[26];
+        isTerminal = false;
+    }
+};
