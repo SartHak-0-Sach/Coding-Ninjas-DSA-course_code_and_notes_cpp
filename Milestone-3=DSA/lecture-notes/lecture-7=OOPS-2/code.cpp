a7f0ff87fd2b22c6178b61c43194044e93c64b51
Sarthak <sarthaksachdev_me21b16_11@dtu.ac.in>
2024-06-16 02:36:20 +0530

    Update code.cpp

---
+++ Milestone-3=DSA/lecture-notes/lecture-7=OOPS-2/code.cpp
@@ -0,0 +1,22 @@
+class Polynomial
+{
+    int *degCoeff;
+    int capacity;
+
+public:
+    // Default Constructor
+
+    // Copy constructor
+
+    // Copy assignment operator
+
+    // print
+
+    // setCoefficient
+
+    // +
+
+    // -
+
+    // *
+};
