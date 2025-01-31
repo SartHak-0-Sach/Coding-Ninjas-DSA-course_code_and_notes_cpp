b00fd53ec41369cb5d9fd55539b687fdae28ea3e
Sarthak <saarsaach30@gmail.com>
2025-01-31 07:10:12 +0530

    Add new DSA problem solution

---
+++ 1/0000000000000488/VehicleUse.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include "Vehicle.cpp"
+#include "Car.cpp"
+
+int main() {
+	Vehicle v;
+
+	v.color = "Blue";
+	// v.maxSpeed = 100;
+	// v.numTyres = 4;
+
+
+	Car c;
+	c.color = "Black";
+//	c.numTyres = 4;
+	c.numGears = 5;
+
+
+}
+
