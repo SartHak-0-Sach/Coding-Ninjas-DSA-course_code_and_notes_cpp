346f32e0543f006a7002fc86c956f7fcea4355e3
Sarthak <saarsaach30@gmail.com>
2025-02-04 07:20:18 +0530

    Define student class code file

---
+++ 1/0000000000000228/Student.cpp
@@ -0,0 +1,34 @@
+class Student {
+
+	public :
+
+	int rollNumber;
+	
+	private :
+
+	int age;
+
+
+	public :
+
+	void display() {
+		cout << age << " " << rollNumber << endl;
+	}
+
+	int getAge() {
+		return age;
+	}
+
+	void setAge(int a, int password) {
+		if(password != 123) {
+			return;
+		}
+		if(a < 0) {
+			return;
+		}
+		age = a;
+	}
+
+};
+
+
