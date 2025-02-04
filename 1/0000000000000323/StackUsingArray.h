8e682922b29488cd4bd822dfe738aeefabeaf94b
Sarthak <saarsaach30@gmail.com>
2025-02-04 07:19:39 +0530

    Define stack declaration class code file

---
+++ 1/0000000000000323/StackUsingArray.h
@@ -0,0 +1,106 @@
+#include <climits>
+
+template <typename T>
+class StackUsingArray {
+	T *data;
+	int nextIndex;
+	int capacity;	
+
+	public :
+
+	StackUsingArray() {
+		data = new T[4];
+		nextIndex = 0;
+		capacity = 4;
+	}
+
+	// return the number of elements present in my stack
+	int size() {
+		return nextIndex;
+	}
+
+	bool isEmpty() {
+		/*
+		if(nextIndex == 0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		*/
+
+		return nextIndex == 0;
+	}
+
+	// insert element
+	void push(T element) {
+		if(nextIndex == capacity) {
+			T *newData = new T[2 * capacity];
+			for(int i = 0; i < capacity; i++) {
+				newData[i] = data[i];
+			}
+			capacity *= 2;
+			delete [] data;
+			data = newData;
+			/*cout << "Stack full " << endl;
+			return;*/
+		}
+		data[nextIndex] = element;
+		nextIndex++;
+	}
+
+	// delete element
+	T pop() {
+		if(isEmpty()) {
+			cout << "Stack is empty " << endl;
+			return 0;	
+		}
+		nextIndex--;
+		return data[nextIndex];
+	}
+	T top() {
+		if(isEmpty()) {
+			cout << "Stack is empty " << endl;
+			return 0;	
+		}
+		return data[nextIndex - 1];
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
