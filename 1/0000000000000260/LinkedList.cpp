70dd47bbf4533e849ac9a5e50a12574d1d56bb06
Sarthak <saarsaach30@gmail.com>
2025-02-06 07:21:58 +0530

    Update linkedList code solution

---
+++ 1/0000000000000260/LinkedList.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include "Node.cpp"
+
+
+int main() {
+	// Statically
+	Node n1(1);
+	Node *head = &n1;
+
+	Node n2(2);
+
+	n1.next = &n2;
+
+	cout << n1.data << " " << n2.data << endl;
+
+
+}
+
